c29e469f995be7d9721a69fab394923b68473d8f
Dmytro Kovtun <d.kovtun@promorepublic.com>
2019-10-22 17:25:04 +0300

    ember-cli-update to 3.12

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  included: function (app) {
+    this._super.included(app);
+    console.log(app);
+
+    if (!process.env.EMBER_CLI_FASTBOOT) {
+      app.import({
+        development: 'node_modules/masonry-layout/dist/masonry.pkgd.js',
+        production: 'node_modules/masonry-layout/dist/masonry.pkgd.min.js'
+      });
+
+      app.import({
+        development: 'node_modules/imagesloaded/imagesloaded.pkgd.js',
+        production: 'node_modules/imagesloaded/imagesloaded.pkgd.min.js'
+      });
+    }
+  }
 };
