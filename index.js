a08a3c6b570bd28573d6d821082380303d6e853a
Dmytro Kovtun <d.kovtun@promorepublic.com>
2019-10-22 17:10:38 +0300

    ember-cli-update to 3.12

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name,
-
-  included: function (app) {
-    this._super.included(app);
-
-    if (!process.env.EMBER_CLI_FASTBOOT) {
-      app.import({
-        development: app.bowerDirectory + '/masonry/dist/masonry.pkgd.js',
-        production: app.bowerDirectory + '/masonry/dist/masonry.pkgd.min.js'
-      });
-
-      app.import({
-        development: app.bowerDirectory + '/imagesloaded/imagesloaded.pkgd.js',
-        production: app.bowerDirectory + '/imagesloaded/imagesloaded.pkgd.min.js'
-      });
-    }
-  }
+  name: require('./package').name
 };
--- tests/integration/components/masonry-grid/component-test.js
+++ tests/integration/components/masonry-grid/component-test.js
@@ -4,6 +4,7 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import $ from 'jquery';
 
 let items;
 
